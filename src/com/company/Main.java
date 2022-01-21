f52d5f35ddeb824fb04bd4c50a358bfd9071a1b4
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-21 18:35:43 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int wholeNumber = 70; //Teilaufgabe a.)
+
+        float weight; //Teilaufgabe b.)
+
+        int age;
+        age = 35; //Teilaufgabe c.)
+
+    }
+}
